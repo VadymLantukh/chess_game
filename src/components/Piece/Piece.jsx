8c56e4a43fd2e86f8b8228ddb0d24011da3f9c14
Vadym_Lantukh <vadym.lantukh.1998@gmail.com>
2024-10-23 12:05:01 +0300

    Fixed the display of figures on the board? and add helpers pieceClasses

--- src/components/Piece/Piece.jsx
+++ src/components/Piece/Piece.jsx
@@ -1,7 +1,10 @@
+import { pieceTypeClass } from "../../helpers/pieceClasses";
 import css from "./Piece.module.css";
 
 const Piece = ({ rank, file, piece }) => {
-  return <li className={`${css.piece} ${piece} p-${rank}${file}`} />;
+  return (
+    <li className={`${css.piece} ${pieceTypeClass[piece]} p-${rank}${file}`} />
+  );
 };
 
 export default Piece;
---
+++ src/helpers/pieceClasses.js
@@ -0,0 +1,16 @@
+import css from "../../src/components/Piece/Piece.module.css";
+
+export const pieceTypeClass = {
+  br: css.br,
+  bk: css.bk,
+  bq: css.bq,
+  bb: css.bb,
+  bp: css.bp,
+  bh: css.bh,
+  wr: css.wr,
+  wk: css.wk,
+  wq: css.wq,
+  wb: css.wb,
+  wp: css.wp,
+  wh: css.wh,
+};
